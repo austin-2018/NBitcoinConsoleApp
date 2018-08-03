cd8cb16f4b7ed5f4b69c0ec2b2aa4e22f1a34433
austin-2018 <sharon.austin.2018@gmail.com>
2018-08-03 17:27:17 -0400

    after adding gitignore

---
+++ NBitcoinConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NBitcoin;
+
+
+namespace NBitcoinConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! " + new Key().GetWif(Network.Main));
+        }
+    }
+}
